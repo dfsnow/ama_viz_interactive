930989dfe79c6b71f9b9ab1e5bd4d3b5f3c23a1f
Dan Snow <snow.dfs@gmail.com>
2019-03-08 19:03:32 +0000

    Added scripts from master

--- map.js
+++ map.js
@@ -8,118 +8,209 @@ var files = [
 // Loading files via promises
 Promise.all(files.map(url => fetch(url)
     .then(data => data.json())))
-    .then(data => mainMap(data));
+    .then(data => mainMap(data))
+    .catch(function(error) {
+        console.log(error);
+    });
 
 function mainMap(data) {
     const [stateShapes, schoolsInfo, schoolsData] = data;
+    const mainDiv = document.getElementById("map-column");
 
-    // Setting up margins
-    const margin = {top: 20, left: 20, right: 20, bottom: 20};
-    const width = 1200 - margin.left - margin.right;
-    const height = 800 - margin.top - margin.bottom;
+    // Setting up margins, width, and height
+    const margin = {top: 20, left: 20, right: 10, bottom: 10};
+    const width = mainDiv.clientWidth - margin.left - margin.right;
+    const height = mainDiv.clientHeight - margin.top - margin.bottom;
 
-    // Setting a geographic projection, path, and bounding box
-    // Contours must be drawn inside this bbox
+    // Setting the projection and path for drawing states
+    // and contours
     var projection = d3.geoAlbersUsa()
-        .fitWidth(width - margin.left - margin.right, stateShapes);
+        .fitWidth(width - margin.left, stateShapes);
 
     var path = d3.geoPath(projection);
-    var bbox = path.bounds(stateShapes);
 
-    // Setting up the draw area for contours
-    var x = d3.scaleLinear()
-        .domain([bbox[0][0], bbox[1][0]])
-        .range([bbox[0][0], bbox[1][0]]);
-
-    var y = d3.scaleLinear()
-        .domain([bbox[0][1], bbox[1][1]])
-        .range([bbox[0][1], bbox[1][1]]);
-
-    // Sequential color scalss for contours
-    var color = d3.scaleSequential(d3.interpolateViridis)
-        .domain([0, 1]);
+    var bbox = path.bounds(stateShapes)
+    var bbox = {
+        xmin: bbox[0][0], xmax: bbox[1][0],
+        ymin: bbox[0][1], ymax: bbox[1][1]
+    };
 
     // Reprojecting lat/lon school locations into albers
     var schoolsInfoTrans = schoolsInfo.map(function(d) {
-        return [d.med_code, d.med_address, projection([d.med_lon, d.med_lat])]
+        var locs = projection([d.med_lon, d.med_lat])
             .flat()
             .filter(function(d) { return d != null; });
-        });
+
+        return {
+            school_code: d.med_code,
+            school_name: d.med_school_name,
+            lon: locs[0],
+            lat: locs[1]
+        };
+    });
 
     var schoolsDataTrans = schoolsData.map(function(d) {
-        return [d.med_school_id, d.geoid, d.docs, projection([d.lon, d.lat])]
+        var locs = [projection([d.lon, d.lat]), 1]
             .flat()
             .filter(function(d) { return d != null; });
-        });
 
-    // Voronoi function
-    //var delaunay = d3.Delaunay
-      //  .from(schoolsDataTrans.map(function(d) { return [d[3], d[4]] }))
-        //.voronoi([bbox[0][0], bbox[0][1], bbox[1][0], bbox[1][1]]);
+        return {
+            school_code: d.med_code,
+            geoid: d.geoid,
+            n_docs: d.docs,
+            lon: locs[0],
+            lat: locs[1]
+        }
+    });
+
+    // Setting up the scales/draw area for contours
+    var x = d3.scaleLinear()
+        .domain([bbox.xmin, bbox.xmax])
+        .range([bbox.xmin, bbox.xmax]);
+
+    var y = d3.scaleLinear()
+        .domain([bbox.ymin, bbox.ymax])
+        .range([bbox.ymin, bbox.ymax]);
+
+    // Sequential color scales for contours
+    var contourColor = d3.scaleSequential(d3.interpolateViridis)
+        .domain([0, 1]);
+
+    // Delaunay function for creating voronoi selection
+    var delaunay = d3.Delaunay
+        .from(schoolsInfoTrans.map(function(d) { return [d.lon, d.lat] }))
+
+    // Creating a temporary div to put the school name into
+    var div = d3.select("#info-container")
+        .append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 1);
 
     // Creating an svg and appending a map + school locations
-    var div = d3.select("body").append("div")
-        .attr("class", "tooltip")
-        .style("opacity", 0);
-
     var svg = d3.select('.map-container')
         .attr('width', width)
         .attr('height', height)
         .append('g')
         .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
+    // Appending the states as static background
     svg.selectAll('.state')
-        .data(stateShapes.features).enter()
-        .append('path')
+        .data(stateShapes.features)
+        .join('path')
         .attr('class', 'state')
         .attr('stroke', 'black')
         .attr('fill', "lightgrey")
         .attr('d', path);
 
+
+    // Initialize a set to hold all selected medical schools
+    var medSchoolSet = new Set();
+
+    // Function which draws contours based on a medical school id
+    // as an input, adds to set of schools until cleared
     var drawContours = function(d) {
-        div.transition()
-            .duration(200)
-            .style("opacity", .9);
 
-        div.html(d[1])
-            .style("left", width - 400 + "px")
-            .style("top", 135 + "px");
+        // Append new values to set of schools
+        medSchoolSet.add(d.school_code);
 
+        // Return only the students from selected med schools
         schoolsFiltered = schoolsDataTrans
-            .filter(function(x) {return x[0] === d[0] })
-            .map(function(d) {return [d[3], d[4], d[2]] });
+            .filter(function(x) { return medSchoolSet.has(x.school_code) })
+            .map(function(d) {
+                return {
+                    lon: d.lon,
+                    lat: d.lat,
+                    weight: d.n_docs
+                }
+            });
 
+        // Draw the contours on the X Y plane of the svg
         var contours = d3.contourDensity()
-            .x(function(d) { return x(d[0]); })
-            .y(function(d) { return y(d[1]); })
-            .weight(function(d) { return d[2]; })
+            .x(function(d) { return x(d.lon); })
+            .y(function(d) { return y(d.lat); })
+            .weight(function(d) { return d.weight; })
             .size([width, height])
             .bandwidth(4)
             (schoolsFiltered);
 
-        d3.selectAll(".contour").remove();
+        //d3.selectAll(".contour").remove();
 
-        svg.insert("g", "g")
-            .attr("fill", "none")
-            .attr("stroke", "#000")
-            .attr("stroke-width", 0)
-            .attr("stroke-linejoin", "round")
-            .attr("opacity", 0.9)
-            .selectAll(".contour")
-            .data(contours).enter()
-            .append("path")
+        // Join the contours to the SVG
+        svg.selectAll(".contour")
+            .data(contours)
+            .join("path")
             .attr('class', 'contour')
-            .attr("fill", function(d) { return color(d.value); })
-            .attr("d", d3.geoPath())
-        };
+            .attr("fill", function(d) { return contourColor(d.value); })
+            .attr("d", d3.geoPath());
+    };
 
+
+    // Function that finds and highlights nearest point
+    var mouseMoveHandler = function() {
+        const [mx, my] = d3.mouse(this);
+        const highlight_point = schoolsInfoTrans[delaunay.find(mx, my)];
+
+        d3.select(this).style("cursor", "pointer");
+
+        div.html(highlight_point.school_name);
+        svg.selectAll(".circle-highlight")
+            .attr("class", "circle-highlight")
+            .attr("cx", highlight_point.lon)
+            .attr("cy", highlight_point.lat)
+            .style("fill", "red")
+    };
+
+
+    // Function that draws contours of nearest point on click
+    var mouseClickHandler = function() {
+        const [mx, my] = d3.mouse(this);
+        const point = delaunay.find(mx, my);
+
+        drawContours(schoolsInfoTrans[point]);
+    };
+
+
+    // Draw all schools on the map
     svg.selectAll("circle")
-        .data(schoolsInfoTrans).enter()
-        .append("circle")
-        .attr("cx", function(d) { return x(d[2]); })
-        .attr("cy", function(d) { return y(d[3]); })
-        .attr("r", 5)
+        .data(schoolsInfoTrans)
+        .join("circle")
+        .attr("cx", function(d) { return x(d.lon); })
+        .attr("cy", function(d) { return y(d.lat); })
+        .attr("r", 4)
         .attr("fill", "slategrey")
-        .on("mouseover", function(d) { drawContours(d) });
+
+    // Draw an arbitrary highlight circle to move to nearest school
+    svg.append('circle')
+        .attr('class', 'circle-highlight')
+        .attr('r', 6)
+        .style('fill', 'none');
+
+    // Draw a rectangle overlay to detect mouse movement
+    svg.append("rect")
+        .attr("class", "overlay")
+        .attr("width", width)
+        .attr("height", height)
+        .style("fill", "black")
+        .style("opacity", 0)
+        .on("mousemove", mouseMoveHandler)
+        .on("click", mouseClickHandler);
+        //.on("mouse", function(d) { drawContours(d) });
 
 };
+
+// Suggestions from Alex
+// - Searchable style dropdown, html box, populated from d3
+// - when click add box to list
+// - drag out selected boxes or deselect on click again
+// - Lasso brush selection
+// - Add note cueing to lasso
+// - Select presets (coasts, midwest, rural, DO)
+
+// TODO
+// Make points permanent on click
+// Add school statistics to sidebar
+// Better contour colors
+// transitions?
+// fix Hawaii
+// add search
+//
